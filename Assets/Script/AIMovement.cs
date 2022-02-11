be952d3025a59b9623fb85c1de07ceb42f4dda77
annpan1123 <99447836+annpan1123@users.noreply.github.com>
2022-02-11 13:30:18 +1100

    Made AI move to A position

--- Assets/Script/AIMovement.cs
+++ Assets/Script/AIMovement.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject position0;
+    public GameObject position1;
 
-    // Update is called once per frame
+
     void Update()
     {
+        Vector2 AiPosition = transform.position;
+
+        // transform.position = Vector2.MoveTowards(current: transform.position, target: position0.transform.position, Time.deltaTime);
+
+
+        //method 1
+        /*
+        if (transform.position.x < position0.transform.position.x) //move Right
+        {
+            //Move Right
+            AiPosition.x += (1 * Time.deltaTime);
+            transform.position = AiPosition;
+        }
+        else
+        {
+            //Move Left
+            AiPosition.x -= (1 * Time.deltaTime);
+            transform.position = AiPosition;
+        }
+
+        //method 2
+        if (transform.position.y < position0.transform.position.y)
+        {
+            transform.position += (Vector3) Vector2.up * 1 * Time.deltaTime;
+        }
+        else
+        {
+            transform.position -= (Vector3) Vector2.up * 1 * Time.deltaTime;
+        } */
         
+
+        //direction from A to B
+        //is B - A
+
+        //method 3
+        Vector2 directionToPos0 = (position0.transform.position - transform.position);
+        directionToPos0.Normalize();
+        transform.position += (Vector3) directionToPos0 * 1 * Time.deltaTime;
     }
 }
